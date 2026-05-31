0d102a087237a03c8d81863d20317f31648e031f
aishvitha-7526 <aishvithaganapuram@gmail.com>
2026-05-31 12:45:16 +0530

    EMAIL_SPAM_CLASSIFIER.py

--- EMAIL_SPAM_CLASSIFIER.py
+++ EMAIL_SPAM_CLASSIFIER.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
-# Sample dataset
 data = {
     'message': [
         'Congratulations! You won a lottery',
@@ -19,30 +18,25 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Features and labels
 X = df['message']
 y = df['label']
 
-# Convert text into numerical form
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(X)
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train model
 model = MultinomialNB()
 model.fit(X_train, y_train)
 
-# Predictions
 y_pred = model.predict(X_test)
 
-# Accuracy
+
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
-# Test a new email
+
 email = ["You have won a free mobile phone"]
 email_vector = vectorizer.transform(email)
 
